d19dd2fe74d2f5abd3ae5e9db3c573c0b8d2a525
kardiatasow664-glitch <kardiatasow664@gmail.com>
2026-03-30 17:10:34 +0000

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,26 @@ function Navbar() {
       <h1>KMS</h1>
       <div className='lien'>
         
-
-       
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/services">Services</NavLink>
-          <NavLink to="/portfolio">Portfolio</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/">
+          <p>Home</p>
+          </NavLink>
+          <NavLink to="/about">
+          <p>About</p>
+          </NavLink>
+          <NavLink to="/services">
+          <p>Services</p>
+          </NavLink>
+          <NavLink to="/portfolio">
+          <p>Portfolio</p>
+          </NavLink>
+          <NavLink to="/contact">
+          <p>Contact</p>
+          </NavLink>
         </div>
       
 
       <div className="menu">
-        <div className="about">About</div>
+        <div className=""></div>
 
         <IoMenu
           onClick={() => setOpenMenu(!openMenu)}
